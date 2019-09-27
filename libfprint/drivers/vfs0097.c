cd55086cbcd1a23db62ceaae05ab36e75d6d5ba6
Alexander Mikhalevich <alex.mikhalevich@gmail.com>
2019-09-27 00:47:12 +0000

    Implement init method

--- libfprint/drivers/vfs0097.c
+++ libfprint/drivers/vfs0097.c
@@ -1,28 +1,54 @@
-#include "validity.h"
+#include "vfs0097.h"
 
 #define FP_COMPONENT "vfs0097"
 
-static int dev_discover(struct libusb_device_descriptor *dsc, uint32_t *devtype) {
-    if (dsc->idProduct == 0x0097)
-        return 1;
+static int dev_discover(struct libusb_device_descriptor *dsc,
+                        uint32_t *devtype) {
+    if (dsc->idProduct == 0x0097) return 1;
     return 0;
 }
 
-static int dev_init(struct fp_img_dev *dev, unsigned long driver_data) {
+static void activate_ssm(fpi_ssm *ssm, struct fp_dev *_dev, void *user_data) {
     // TODO
+}
+
+static void dev_open_callback(fpi_ssm *ssm, struct fp_dev *_dev,
+                              void *user_data) {
+    /* Notify open complete */
+    fpi_imgdev_open_complete(user_data, 0);
+    fpi_ssm_free(ssm);
+}
+
+static int dev_init(struct fp_img_dev *dev, unsigned long driver_data) {
+    struct vfs_0097_dev_t *vdev;
+
+    /* Claim usb interface */
+    int error = libusb_claim_interface(fpi_dev_get_usb_dev(FP_DEV(dev)), 0);
+    if (error < 0) {
+        /* Interface not claimed, return error */
+        fp_err("could not claim interface 0");
+        return error;
+    }
+    vdev = g_malloc0(sizeof(struct vfs_0097_dev_t));
+    fp_dev_set_instance_data(FP_DEV(dev), vdev);
+
+    /* Clearing previous device state */
+    fpi_ssm *ssm =
+        fpi_ssm_new(FP_DEV(dev), activate_ssm, SSM_STATES_AMOUNT, dev);
+    fpi_ssm_start(ssm, dev_open_callback);
     return 0;
 }
 
 static void dev_deinit(struct fp_img_dev *dev) {
-	void *user_data;
-	user_data = FP_INSTANCE_DATA(FP_DEV(dev));
-	g_free(user_data);
-	libusb_release_interface(fpi_dev_get_usb_dev(FP_DEV(dev)), 0);
-	fpi_imgdev_close_complete(dev);
+    void *user_data;
+    user_data = FP_INSTANCE_DATA(FP_DEV(dev));
+    g_free(user_data);
+    libusb_release_interface(fpi_dev_get_usb_dev(FP_DEV(dev)), 0);
+    fpi_imgdev_close_complete(dev);
 }
 
 static int dev_activate(struct fp_img_dev *dev) {
-    //TODO
+    // TODO
     return 0;
 }
 
@@ -31,19 +57,24 @@ static void dev_deactivate(struct fp_img_dev *dev) {
 }
 
 static const struct usb_id id_table[] = {
-	{ .vendor = 0x138a, .product = 0x0097 },
-	{ 0, 0, 0, },
+    {.vendor = 0x138a, .product = 0x0097},
+    {
+        0,
+        0,
+        0,
+    },
 };
 
 struct fp_img_driver validity_driver = {
-    .driver = {
-        .id = VFS0097_ID,
-        .name = FP_COMPONENT,
-        .full_name = "Validity VFS0097",
-        .id_table = id_table,
-        .scan_type = FP_SCAN_TYPE_SWIPE,
-        .discover = dev_discover,
-    },
+    .driver =
+        {
+            .id = VFS0097_ID,
+            .name = FP_COMPONENT,
+            .full_name = "Validity VFS0097",
+            .id_table = id_table,
+            .scan_type = FP_SCAN_TYPE_SWIPE,
+            .discover = dev_discover,
+        },
     .flags = 0,
     .img_width = -1,
     .img_height = -1,
--- libfprint/drivers/vfs0097.h
+++ libfprint/drivers/vfs0097.h
@@ -1,1 +1,62 @@
+#include <stdbool.h>
+
 #include "drivers_api.h"
+
+/* SSM states for activate_ssm */
+enum SSM_STATE {
+    /*
+    SSM_INITIAL_ABORT_1,
+    SSM_INITIAL_ABORT_2,
+    SSM_INITIAL_ABORT_3,
+    SSM_CLEAR_EP2,
+    SSM_TURN_OFF,
+
+    SSM_TURN_ON,
+
+    SSM_ASK_INTERRUPT,
+    */
+    SSM_WAIT_INTERRUPT,
+    SSM_RECEIVE_FINGER,
+    /*
+    SSM_SUBMIT_IMAGE,
+
+    SSM_NEXT_RECEIVE,
+    SSM_WAIT_ANOTHER_SCAN,
+    */
+
+    SSM_STATES_AMOUNT
+};
+
+/* The main driver structure */
+struct vfs_0097_dev_t {
+    /* One if we were asked to read fingerprint, zero otherwise */
+    bool active;
+
+    /* Control packet parameter for send_control_packet */
+    uint8_t *control_packet;
+
+    /* For dev_deactivate to check whether ssm still running or not */
+    bool ssm_active;
+
+    /* Current async transfer */
+    struct libusb_transfer *transfer;
+
+    /* Should we call fpi_imgdev_activate_complete or
+     * fpi_imgdev_deactivate_complete */
+    bool need_report;
+
+    /* Should we wait more for interrupt */
+    bool wait_interrupt;
+
+    /* Received fingerprint raw lines */
+    struct vfs_line *lines_buffer;
+
+    /* Current number of received bytes and current memory used by data */
+    uint32_t bytes, memory;
+
+    /* USB buffer for fingerprint */
+    uint8_t *usb_buffer;
+
+    /* Received interrupt data */
+    uint8_t interrupt[8];
+};
